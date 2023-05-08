e95b482dc108266ac8bfbda2517adafad2fc1fb6
hye914 <kimhyesu0914@gmail.com>
2023-05-08 21:40:18 +0900

    submit

--- likelion11th/urls.py
+++ likelion11th/urls.py
@@ -14,15 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('main.urls')),
     path('', views.mainpage, name="mainpage"),
     path('second/', views.secondpage, name="secondpage"),
     path('new/', views.new, name="new"),
     path('create/', views.create, name="create"),
     path('<int:id>', views.detail, name="detail"),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -19,10 +19,31 @@ def create(request):
     new_blog.writer = request.POST['writer']
     new_blog.pub_date = timezone.now()
     new_blog.body = request.POST['body']
-
+    new_blog.image=request.FILES.get('image')
     new_blog.save()
     
-    return redirect('detail', new_blog.id)
+    return redirect('main:detail', new_blog.id)
 
 def new(request):
     return render(request, 'main/new.html')
+
+def edit(request,id):
+    edit_blog=Blog.objects.get(id=id)
+    return render(request,'main/edit.html',{'blog':edit_blog})
+
+#update기능 구현 함수
+def update(request,id):
+    update_blog=Blog.objects.get(id=id)
+    update_blog.title=request.POST['title']
+    update_blog.writer=request.POST['writer']
+    update_blog.pub_date=timezone.now()
+    update_blog.body=request.POST['body']
+    update_blog.save()
+    return redirect('main:detail', update_blog.id)
+
+#delete구현
+
+def delete(request,id):
+    delete_blog=Blog.objects.get(id=id)
+    delete_blog.delete()
+    return redirect('main:mainpage')
